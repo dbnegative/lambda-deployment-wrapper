34978c1a62d6ba678d5734cdd2eee68c6962508e
Jason Witting <jason.witting@MacBook-Pro-5.local>
2016-10-31 21:39:18 +0000

    Improved build script vars

--- build.sh
+++ build.sh
@@ -1,39 +1,30 @@
 #!/bin/bash
-FUNCTION_NAME="CHANGEME" #aka cloudfront-log-ingester
 
-
-# VirtualEnv vars
-VENV="_venv"
-VENV_LIBS=${VENV}/lib/python2.7/site-packages
-BASE=`pwd`
-
-#Build Vars
-NOW=`date`
-SUFFIX=`date -j -f "%a %b %d %T %Z %Y" "${NOW}" "+%s"`
-PACKAGE_NAME="${FUNCTION_NAME}-${SUFFIX}"
-SERVICE_FILE="lambda_function.py"
-BUILD_LOG="${BASE}/build-${SUFFIX}.log"
+#source build vars
+source config/build.conf
 
 function cleanup {
-    echo "Destroying environment...."
+    echo "Destroying environment..."
     rm -rf ./${VENV}
     rm -rf ./*.zip 
     rm -rf ./*.log 
 }
 
 function setup { # ->  Creates a new vurtual python enviroment
-    echo "[+] Creating environment \n [$NOW]"  >> ${BUILD_LOG}
+    echo "[+] Creating environment... [$NOW]"  >> ${BUILD_LOG}
     cd ${BASE} 
     virtualenv ${VENV}  >> ${BUILD_LOG}
     source ${VENV}/bin/activate  >> ${BUILD_LOG}
-    pip install elasticsearch  >> ${BUILD_LOG}
-    pip install aws_requests_auth  >> ${BUILD_LOG}
+    for i in "${pkg_list[@]}"
+    do
+        pip install "$i" >> ${BUILD_LOG}
+    done
     mkdir ${VENV}/dist  >> ${BUILD_LOG}
 }
 
 function build {
     # -> Packages lambda function into acceptable ZIP format
-    echo "[+] Building Lambda package at ${BASE}/${PACKAGE_NAME}.zip [$NOW]"  >> ${BUILD_LOG}
+    echo "[+] Building Lambda package at ${BASE}/${PACKAGE_NAME}.zip... [$NOW]"  >> ${BUILD_LOG}
     rm -rf ${VENV}/dist/*  >> ${BUILD_LOG}
     cp -R ${VENV_LIBS}/* ${VENV}/dist/  >> ${BUILD_LOG}
     cp ${SERVICE_FILE} ${VENV}/dist/  >> ${BUILD_LOG}
