5b81352eb4099df935053ba2e795cc351914da13
Jason Witting <jason.witting@MacBook-Pro-5.local>
2016-10-31 21:07:09 +0000

    improved variable naming consistency

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ NOW=`date`
 SUFFIX=`date -j -f "%a %b %d %T %Z %Y" "${NOW}" "+%s"`
 PACKAGE_NAME="${FUNCTION_NAME}-${SUFFIX}"
 SERVICE_FILE="lambda_function.py"
-BUILDLOG="${BASE}/build-${SUFFIX}.log"
+BUILD_LOG="${BASE}/build-${SUFFIX}.log"
 
 function cleanup {
     echo "Destroying environment...."
@@ -22,22 +22,22 @@ function cleanup {
 }
 
 function setup { # ->  Creates a new vurtual python enviroment
-    echo "[+] Creating environment \n [$NOW]"  >> ${BUILDLOG}
+    echo "[+] Creating environment \n [$NOW]"  >> ${BUILD_LOG}
     cd ${BASE} 
-    virtualenv ${VENV}  >> ${BUILDLOG}
-    source ${VENV}/bin/activate  >> ${BUILDLOG}
-    pip install elasticsearch  >> ${BUILDLOG}
-    pip install aws_requests_auth  >> ${BUILDLOG}
-    mkdir ${VENV}/dist  >> ${BUILDLOG}
+    virtualenv ${VENV}  >> ${BUILD_LOG}
+    source ${VENV}/bin/activate  >> ${BUILD_LOG}
+    pip install elasticsearch  >> ${BUILD_LOG}
+    pip install aws_requests_auth  >> ${BUILD_LOG}
+    mkdir ${VENV}/dist  >> ${BUILD_LOG}
 }
 
 function build {
     # -> Packages lambda function into acceptable ZIP format
-    echo "[+] Building Lambda package at ${BASE}/${PACKAGE_NAME}.zip [$NOW]"  >> ${BUILDLOG}
-    rm -rf ${VENV}/dist/*  >> ${BUILDLOG}
-    cp -R ${VENV_LIBS}/* ${VENV}/dist/  >> ${BUILDLOG}
-    cp ${SERVICE_FILE} ${VENV}/dist/  >> ${BUILDLOG}
-    cd ${VENV}/dist && zip -r ${BASE}/${PACKAGE_NAME}.zip *  >> ${BUILDLOG}
+    echo "[+] Building Lambda package at ${BASE}/${PACKAGE_NAME}.zip [$NOW]"  >> ${BUILD_LOG}
+    rm -rf ${VENV}/dist/*  >> ${BUILD_LOG}
+    cp -R ${VENV_LIBS}/* ${VENV}/dist/  >> ${BUILD_LOG}
+    cp ${SERVICE_FILE} ${VENV}/dist/  >> ${BUILD_LOG}
+    cd ${VENV}/dist && zip -r ${BASE}/${PACKAGE_NAME}.zip *  >> ${BUILD_LOG}
     echo "${PACKAGE_NAME}.zip"
 }
 
